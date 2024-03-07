21061655fcd3a85c457405b1539cde9983e76065
Leonardo Pinto Passeri <leoppasseri@gmail.com>
2024-03-07 16:12:32 -0300

    att

---
+++ CursoEmVideo/Aula1.java
@@ -0,0 +1,8 @@
+package CursoEmVideo;
+
+public class Aula1{ //Classe
+    public static void main(String [] args){ //método "main"
+        System.out.println("Olá Mundo Java"); //println => Printa e pula uma linha
+        System.out.print("Olá Mundo Java sem pular uma linha");
+    }
+}
