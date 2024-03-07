6ac5cee39f435b1ff37116e8bff9d6306089a5a9
Leonardo Pinto Passeri <leoppasseri@gmail.com>
2024-03-07 16:18:03 -0300

    att

--- CursoEmVideo/Aula1.java
+++ CursoEmVideo/Aula1.java
@@ -1,6 +1,9 @@
-package CursoEmVideo;
+package cursoemvideo;
 
 public class Aula1{ //Classe
+    //Primeira letra em MAIUSCULO quer dizer que é CLASSE 
+    //Atribulo/variavel/metodo é por exemplo, meuAtribulo. Com as letras do meio em maiusculo
+
     public static void main(String [] args){ //método "main"
         System.out.println("Olá Mundo Java"); //println => Printa e pula uma linha
         System.out.print("Olá Mundo Java sem pular uma linha");
