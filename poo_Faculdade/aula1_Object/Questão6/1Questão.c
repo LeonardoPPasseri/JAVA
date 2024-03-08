4a3e15c6366dd85242bedca3499fb8b43189d9ec
Leonardo Pinto Passeri <leoppasseri@gmail.com>
2024-03-08 15:47:33 -0300

    att

---
+++ poo_Faculdade/aula1_Object/Questão6/1Questão.c
@@ -0,0 +1,10 @@
+/*
+Exercício 6 – Crie uma nova classe chamada TestaCasaJanela com o método
+main.
+A- Crie um novo objeto Janela e atribua valores a ele.
+B- Crie um novo objeto Casa e atribua valores a ele, inclusive a Janela criada
+acima (item A).
+C- Faça o debug e entenda o que acontece.
+D- A partir da referencia da Casa, imprima os valores dos atributos da casa e
+da janela.
+*/
