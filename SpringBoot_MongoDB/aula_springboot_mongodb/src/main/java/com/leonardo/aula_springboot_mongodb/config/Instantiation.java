8143431ff0b013ce44b7cfc02b440e4e198bb5e9
Leonardo <leoppasseri@gmail.com>
2026-02-17 16:35:58 -0300

    Add Post entity and seed posts on startup
    Introduce Post domain and repository, and update startup instantiation to seed sample posts. Added Post.java (entity with id, date, title, body, author) and PostRepository. Updated Instantiation to import Post/PostRepository, configure SimpleDateFormat with GMT, clear user and post collections, save sample users, create six sample posts with parsed dates and persist them. Also minor comment/formatting tweaks.

--- SpringBoot_MongoDB/aula_springboot_mongodb/src/main/java/com/leonardo/aula_springboot_mongodb/config/Instantiation.java
+++ SpringBoot_MongoDB/aula_springboot_mongodb/src/main/java/com/leonardo/aula_springboot_mongodb/config/Instantiation.java
@@ -1,31 +1,61 @@
 package com.leonardo.aula_springboot_mongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.leonardo.aula_springboot_mongodb.domain.Post;
 import com.leonardo.aula_springboot_mongodb.domain.User;
+import com.leonardo.aula_springboot_mongodb.repository.PostRepository;
 import com.leonardo.aula_springboot_mongodb.repository.UserRepository;
 
-@Configuration //Essa classe é de configuração e deve ser gerenciada pelo Spring
+@Configuration // Essa classe é de configuração e deve ser gerenciada pelo Spring
 public class Instantiation implements CommandLineRunner {
-    // CommandLineRunner é uma interface do Spring Boot que indica que a classe deve executar o método run() quando a aplicação for iniciada. Isso é útil para realizar tarefas de inicialização, como popular o banco de dados com dados de teste.
+    // CommandLineRunner é uma interface do Spring Boot que indica que a classe deve
+    // executar o método run() quando a aplicação for iniciada. Isso é útil para
+    // realizar tarefas de inicialização, como popular o banco de dados com dados de
+    // teste.
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        format.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll(); // Limpa a coleção de usuários no MongoDB antes de inserir os novos dados
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(maria, alex, bob)); // Salva os usuários no MongoDB usando o método
+                                                                 // saveAll() do repositório
 
-        userRepository.saveAll(Arrays.asList(maria,alex,bob)); // Salva os usuários no MongoDB usando o método saveAll() do repositório
+        postRepository.deleteAll();
         
+        Post post1 = new Post(null, format.parse("21/03/2018"), "Partiu viagem!",
+                "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, format.parse("15/06/2019"), "Bom dia!",
+                "Acordei cedo hoje e fui correr no parque.", maria);
+        Post post3 = new Post(null, format.parse("02/01/2020"), "Novo ano",
+                "Que 2020 venha com muitas conquistas e saúde!", alex);
+        Post post4 = new Post(null, format.parse("10/12/2021"), "Formatura",
+                "Finalmente me formei! Obrigado a todos que me apoiaram.", alex);
+        Post post5 = new Post(null, format.parse("05/07/2022"), "Trabalho novo",
+                "Comecei hoje em um novo emprego. Muito animado!", bob);
+        Post post6 = new Post(null, format.parse("18/11/2023"), "Fim de semana",
+                "Nada melhor que descansar e assistir uma boa série.", bob);
+
+        postRepository.saveAll(Arrays.asList(post1, post2, post3, post4, post5, post6));
     }
 
 }
